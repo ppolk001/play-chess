2120b61b64eff00270083d8193087322a839569e
patrick <97654967+ppolk001@users.noreply.github.com>
2022-03-11 12:35:45 -0800

    Create Move.h

---
+++ Move.h
@@ -0,0 +1,27 @@
+//class declarations
+#ifndef __MOVE_HEADER__
+#define __MOVE_HEADER__
+
+#include <string>
+using namespace std;
+//iterator
+
+class Move {
+  private:
+    char ps[64];
+    int indexFrom;
+    int indexTo;
+
+    bool KnightReader(int, int, int);
+    bool BishopReader(int, int, int);
+  
+  public:
+    Move(char (&pieces)[64]);
+    void UpdateMoveIt(char (&pieces)[64]);
+    bool isLegalMove(string, int);
+    int gridConvert(string);
+    int getIndexFrom();
+    int getIndexTo();
+
+};
+#endif
